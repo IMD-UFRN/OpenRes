844f8bae06c142c9e0f0c8c5bc28b4949d413c20
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-12-02 23:40:53 -0300

    Adding translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+
+  def set_locale
+    if params[:locale].blank?
+      I18n.locale = :'pt-BR'
+    else
+      I18n.locale = params[:locale]
+    end
+  end
+
   def authenticate_admin_user!
     authenticate_user! 
     unless current_user.admin?
@@ -14,5 +25,10 @@ class ApplicationController < ActionController::Base
   def current_admin_user
     return nil if user_signed_in? && !current_user.admin?
     current_user
+  end 
+
+  # ensure locale persists
+  def default_url_options(options={})
+    {:locale => I18n.locale}
   end
 end
