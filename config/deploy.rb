3dc33a68ecb09efdce5cb46e622b6e5a676a2843
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2014-02-17 02:10:47 -0300

    Last try by today...

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,23 +34,33 @@ ssh_options[:forward_agent] = true
 
 after 'deploy', 'deploy:cleanup' # keep only the last 5 releases
 
-namespace :assets do
-  desc 'Run the precompile task locally and rsync with shared'
-  task :precompile do
-    run_locally('rm -rf public/assets/*')
-    run_locally("RAILS_ENV=#{rails_env} rake assets:precompile")
-    run_locally('touch assets.tgz && rm assets.tgz')
-    run_locally('tar zcvf assets.tgz public/assets/')
-    run_locally('mv assets.tgz public/assets/')
-  end
+before 'deploy:finalize_update', 'deploy:assets:symlink'
+after 'deploy:update_code', 'deploy:assets:precompile'
 
-  desc 'Upload precompiled assets'
-  task :upload_assets do
-    upload "public/assets/assets.tgz", "#{release_path}/assets.tgz"
-    run "cd #{release_path}; tar zxvf assets.tgz; rm assets.tgz"
+namespace :deploy do
+  namespace :assets do
+
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
+
+    task :symlink, roles: :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+            mkdir -p #{latest_release}/public &&
+            mkdir -p #{shared_path}/assets &&
+            ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    end
   end
 end
 
-before 'deploy:update_code', 'assets:precompile'
-after 'deploy:create_symlink', 'assets:upload_assets'
 # postgres db pass ELnGJfiKUBPe88jPvAfgM
