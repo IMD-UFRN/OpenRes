3fcb24573b59662f09d28c67eeb3ceb79677f169
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-03-07 00:36:25 -0300

    Homepage now is the login page. Closes #70

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@
 OpenRes::Application.routes.draw do
   resources :room_types
 
-  root to: 'home#index'
+  devise_scope :user do
+  root to: "devise/sessions#new"
+end
+
 
   get "reservations/all", to: "reservations#all", as: :all
   get "reservations/finished", to: "reservations#finished", as: :finished
@@ -13,7 +16,7 @@ OpenRes::Application.routes.draw do
   get "reservations/basic_future_reservations", to: "reservations#user_future_reservations",as: :user_future_reservations
 
   devise_for :users
-  
+
   resources :places
   resources :sectors
   resources :reservations
@@ -25,7 +28,7 @@ OpenRes::Application.routes.draw do
   post "reservations/:reservation_id/approve", to: "reservation_approval#approve", as: :reservation_approve
   post "check_reservations/:reservation_id/reject", to: "reservation_approval#reject", as: :reservation_reject
   post "check_reservations/:reservation_id/suspend", to: "reservation_approval#suspend", as: :reservation_suspend
-  
+
   get "reservations/:reservation_id/reject" => 'reservation_approval#justify_status', as: :justify_reject
   get "reservations/:reservation_id/suspend" => 'reservation_approval#justify_status', as: :justify_suspend
 
