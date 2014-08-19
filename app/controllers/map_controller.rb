0cb54f3256b92f8ab8723b842f3bec93aeaef6db
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-08-19 11:08:43 -0300

    Add search for place at map #102

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,12 @@
 class MapController < ApplicationController
   def show
+    @hash ={}
+    if params[:place]
+      Place.where(code: params[:place][:code].upcase).pluck(:code).each do |x|
+        @hash[x]= "#729ae3"
+      end
+    end
+
+    @result= @hash.to_json
   end
 end
