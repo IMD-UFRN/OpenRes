f0593f076b008c002a5e8a628aec45180e44a062
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-08-20 09:51:05 -0300

    Add flash notice for places not found #102

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,9 +1,13 @@
+# -*- encoding : utf-8 -*-
 class MapController < ApplicationController
   def show
     @hash ={"background" => "#696969"}
     if params[:place]
       Place.where(code: params[:place][:code].capitalize).pluck(:code).each do |x|
         @hash[x]= "#729ae3"
+      end
+      if @hash.length == 1
+        flash[:error] = "Sala não encontrada"
       end
     end
 
--- app/decorators/map_decorator.rb
+++ app/decorators/map_decorator.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class MapDecorator < Draper::Decorator
   delegate_all
 
