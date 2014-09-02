a8e8bec8e6464ddb6a752f0c952798700259b683
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-09-02 10:09:36 -0300

    Reservation group name now shows at reservation new

--- app/decorators/reservation_decorator.rb
+++ app/decorators/reservation_decorator.rb
@@ -125,4 +125,5 @@ class ReservationDecorator < Draper::Decorator
     link_to 'Excluir', object, method: :delete, data: { confirm: 'Você tem certeza que deseja excluir esta reserva?' }, class:"btn-small btn-normal"
   end
 
+
 end
