86b405d69b7b525a5482dc3c2f56f906459d8003
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-09-23 10:37:58 -0300

    Update migration to fill empty created_by

--- db/migrate/20140923123501_add_created_by_to_reservations.rb
+++ db/migrate/20140923123501_add_created_by_to_reservations.rb
@@ -1,5 +1,15 @@
 class AddCreatedByToReservations < ActiveRecord::Migration
   def change
     add_column :reservations, :created_by_id, :integer
+
+    Reservation.all.each do |r|
+
+      if r.created_by.nil?
+        r.created_by_id = r.user_id
+
+        r.save
+      end
+    end
+
   end
 end
