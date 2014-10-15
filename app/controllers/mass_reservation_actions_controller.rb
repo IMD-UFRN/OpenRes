26293396a094a8c1bb6ab4a8377da496d4119fa3
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-10-15 10:59:54 -0300

    Add error notice at multiple reservations mass approval

--- app/controllers/mass_reservation_actions_controller.rb
+++ app/controllers/mass_reservation_actions_controller.rb
@@ -1,14 +1,21 @@
+# -*- encoding : utf-8 -*-
 class MassReservationActionsController < ApplicationController
 
   def approve
 
     reservations = params[:reservations].split(",").delete_if{|k| params[:reservations][k].blank? }.map(&:to_i)
 
+    s = 0
+    f = 0
+
     reservations.each do |reservation|
-      ReservationPolicy.approve(Reservation.find(reservation))
+
+      ReservationPolicy.approve(Reservation.find(reservation)).empty? ? s = s + 1 : f = f + 1
+
     end
 
-    flash[:notice] = "Reservas aprovadas com sucesso."
+    flash[:notice] = "#{s} reserva(s) aprovadas com sucesso."
+    flash[:error] = "#{f} reserva(s) não puderam ser aprovadas." if (f > 0)
 
     render :js => 'window.location.reload()'
 
